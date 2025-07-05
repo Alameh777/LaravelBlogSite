7412abe1c025442020d6fd883ec40743a0ffe9ad
Alameh777 <alamehmhmd5@gmail.com>
2025-07-06 01:26:41 +0300

    updated the blogs model

--- app/Models/blogs.php
+++ app/Models/blogs.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class blogs extends Model
 {
-    //
+    use HasFactory;
+
+    // ✅ These are the fields you can mass assign
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'slug',
+        'image',
+    ];
+
+    // ✅ Blog belongs to a user (author)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // ✅ Blog has many comments
+   
 }
